d0aa7a6aa0871954467c46fb247600d590297a81
Cecimartinez <martinezcecilia2908@gmail.com>
2022-05-29 01:56:26 -0300

    modificaed responsive

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 const menu = document.querySelector(".menu");
 const openMenuBtn = document.querySelector(".open-menu");
 const closeMenuBtn = document.querySelector(".close-menu");
@@ -6,6 +5,28 @@ const closeMenuBtn = document.querySelector(".close-menu");
 function toggleMenu(){
   menu.classList.toggle("menu_opened");
 }
-// al hacer click se ejecuta la function
+
 openMenuBtn.addEventListener("click", toggleMenu); 
 closeMenuBtn.addEventListener("click", toggleMenu); 
+
+// validación-email
+
+const email= document.getElementById("email");
+const form =document.getElementById("form");
+const parrafo =document.getElementById("warnings");
+
+form.addEventListener("submit", e=> {
+  e.preventDefault()
+  let warnings=""
+  let regexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  let entrar=false
+  parrafo.innerHTML=""
+  if (!regexEmail.test(email.Value)){
+    warnings+= "El E-mail no es válido"
+  }
+  if (entrar){
+    parrafo.innerHTML= warnings
+  }else{
+    parrafo.innerHTML="Enviado"
+  }
+})
